8223e8502635213fe45a59adfb2f3c5896e27f28
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-12-12 15:45:08 +0200

    add styled-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import { messageSelector, addMessage } from '../modules/notification';
+
+const Container = styled.div`
+  margin: 100px;
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+  background: #ccc;
+`;
 
 class Home extends Component {
   state = {
@@ -25,6 +34,7 @@ class Home extends Component {
         <button onClick={() => this.props.addMessage('CHANGE MESSAGE')}>
           ADD MESSAGE
         </button>
+        <Container />
       </div>
     );
   }
